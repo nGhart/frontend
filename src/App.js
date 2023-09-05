1b096130b5ec4a4d7bfc62ee2da083950d43bb8c
nGhart <naaghart@gmail.com>
2023-09-05 11:32:40 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [animals, setAnimals] = useState(null);
+
+  useEffect(() => {
+    getAnimals();
+  }, []);
+  const getAnimals = async () => {
+    const response = await axios.get('http://localhost:1994/animals');
+    console.log(response);
+    setAnimals(response.data.animals);
+    console.log(response);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {animals &&
+        animals.map((item) => {
+          return (
+            <>
+              <table>
+                <th>
+                  <td>ID</td>
+                  <td>BREED</td>
+                  <td>SEX</td>
+                  <td>BRED</td>
+                  <td>DOB</td>
+                  <td>AGE</td>
+                  <td>WEANED</td>
+                  <td>WEIGHT</td>
+                </th>
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.breed}</td>
+                  <td>{item.sex}</td>
+                  <td>{item.bred}</td>
+                  <td>{item.dob}</td>
+                  <td>Calc</td>
+                  <td>{item.weaning}</td>
+                  <td>{item.weightCurrent}</td>
+                </tr>
+              </table>
+            </>
+          );
+        })}
     </div>
   );
 }
