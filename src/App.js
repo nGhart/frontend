6695113c096de842615bb7bce708d9c553d7bac0
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-23 21:28:32 +0000

    Update App.js removed requireauth

--- src/App.js
+++ src/App.js
@@ -67,12 +67,12 @@ const router = createBrowserRouter(
       <Route
         path="/"
   
-        element={
-          <RequireAuth>
-            <Layout />
-          </RequireAuth>
-        }
-         // element={<Layout />}
+        // element={
+        //   <RequireAuth>
+        //     <Layout />
+        //   </RequireAuth>
+        // }
+        element={<Layout />}
       >
         <Route path="/" element={<Home />} />
         <Route path="animals" element={<AnimalTable />}></Route>
