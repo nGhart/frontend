764b5a4f42685ad4900e3324d2c80143d206595f
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-25 17:40:53 +0000

    Update RequireAuth.js

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -7,17 +7,16 @@ import ErrorPage from './ErrorPage';
 const RequireAuth = (props) => {
   const store = authStore();
   const [error, setError] = useState(null);
-  //deploy
-  // useEffect(() => {
-  //   if (store.loggedIn === null) {
-  //     store.checkAuth().catch((error) => {
-  //       setError(error.message);
-  //     });
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (store.loggedIn === null) {
+      store.checkAuth().catch((error) => {
+        setError(error.message);
+      });
+    }
+  }, []);
   if (store.loggedIn === null) {
-    // return <div>Loading</div>;
-    return <Loading />;
+    return <div>Loading</div>;
+    //return <Loading />;
   }
   //if not logged in redirect to log in page
   if (store.loggedIn === false) {
