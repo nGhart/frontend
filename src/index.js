7a6d3ed68962dcdf9fef3282b321bc58dc34f65a
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-28 19:30:09 +0000

    Update index.js back

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { persistor, store } from './store/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-//axios.defaults.baseURL = 'https://server2-763m.onrender.com';
-axios.defaults.baseURL = 'http://localhost:1995';
+axios.defaults.baseURL = 'https://server2-763m.onrender.com';
+//axios.defaults.baseURL = 'http://localhost:1995';
 //axios.defaults.baseURL = 'https://test-pwvq.onrender.com/';
 //axios.defaults.baseURL = 'https://recordsserver.onrender.com';
 axios.defaults.withCredentials = true;
